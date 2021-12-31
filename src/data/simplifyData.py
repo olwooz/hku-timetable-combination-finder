078b93c4aef3f69b2809d626dd5e0b647a67b649
dyi919 <ehddjs13579@gmail.com>
2022-01-01 02:08:30 +0900

    Updated simplifyData to change JSON format, Added find class combinations function

--- src/data/simplifyData.py
+++ src/data/simplifyData.py
@@ -1,30 +1,48 @@
 import json
 import os
 
-def remove_duplicates(courses):
-    simplified = {
+def divide_subclasses(courses):
+    divided = {
         "code": courses[0]["COURSE CODE"],
         "title": courses[0]["COURSE TITLE"],
         "term": courses[0]["TERM"][-1],
-        "section": courses[0]["CLASS SECTION"],
-        "stime": [courses[0]["START TIME"]] if not courses[0]["START TIME"].isspace() else ["N/A"],
-        "etime": [courses[0]["END TIME"]],
-        "venue": [courses[0]["VENUE"]] if (not courses[0]["VENUE"].isspace()) and (len(courses[0]["VENUE"])>0) else ["N/A"],
+        "subclass": [{
+            "section": courses[0]["CLASS SECTION"],
+            "stime": [courses[0]["START TIME"]] if not courses[0]["START TIME"].isspace() else ["N/A"],
+            "etime": [courses[0]["END TIME"]],
+            "venue": [courses[0]["VENUE"]] if (not courses[0]["VENUE"].isspace()) and (len(courses[0]["VENUE"])>0) else ["N/A"],
+        }]
     }
     day = list(filter(None, [courses[0]["SUN"],courses[0]["MON"],courses[0]["TUE"],courses[0]["WED"],courses[0]["THU"],courses[0]["FRI"],courses[0]["SAT"]]))
     if len(day) > 0:
-        simplified["day"] = [day[0]]
+        divided["subclass"][0]["day"] = [day[0]]
     else:
-        simplified["day"] = ["N/A"]
-    for course in courses:
+        divided["subclass"][0]["day"] = ["N/A"]
+    for course in courses[1:]:
         day = list(filter(None, [course["SUN"],course["MON"],course["TUE"],course["WED"],course["THU"],course["FRI"],course["SAT"]]))
-        if len(day) == 1 and day[0] not in simplified["day"]:
-            simplified["stime"].append(course["START TIME"])
-            simplified["etime"].append(course["END TIME"])
-            if (not course["VENUE"].isspace()) and (len(course["VENUE"])>0):
-                simplified["venue"].append(course["VENUE"])
-            simplified["day"].append(day[0])
-    return simplified
+        exists = False
+        for sub in divided["subclass"]:
+            if course["CLASS SECTION"] == sub["section"]:
+                exists = True
+                if len(day) > 0 and day[0] not in sub["day"]:
+                    sub["stime"].append(course["START TIME"])
+                    sub["etime"].append(course["END TIME"])
+                    sub["venue"].append(course["VENUE"])
+                    sub["day"].append(day[0])
+
+        if not exists:
+            newsub = {
+                "section": course["CLASS SECTION"],
+                "stime": [course["START TIME"]],
+                "etime": [course["END TIME"]],
+                "venue": [course["VENUE"]]
+            }
+            if len(day) > 0:
+                newsub["day"] = [day[0]]
+            else:
+                newsub["day"] = ["N/A"]
+            divided["subclass"].append(newsub)
+    return divided
 
 here = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(here, 'hku_21-22_class_timetable.json')
@@ -41,7 +59,7 @@ for course in courses:
     if prevCourse["COURSE CODE"] == course["COURSE CODE"]:
         sameCourse.append(course)
     else:
-        newJSON.append(remove_duplicates(sameCourse))
+        newJSON.append(divide_subclasses(sameCourse))
         sameCourse.clear()
         sameCourse.append(course)
         prevCourse = course
@@ -49,5 +67,3 @@ for course in courses:
 
 with open(result_filename, "w") as outfile:
     json.dump(newJSON, outfile)
-
-
--- src/routes/Timetable.js
+++ src/routes/Timetable.js
@@ -3,19 +3,93 @@ import Select from 'react-select'
 
 import courses from "../data/simplified";
 
+// check if two courses have overlapping time
+function compareTime(c1, c2) { 
+    for (var i = 0; i < c1["day"].length; i++) {
+        for (var j = 0; j < c2["day"].length; j++) {
+            if (c1["day"][i] == c2["day"][j]) {
+                if (c1["stime"][i] < c2["etime"][j] && c1["stime"][i] >= c2["stime"][j]) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+// check if there are no same courses and no time clash in the combination
+function isValidCombination(courses) {
+    for (var i = 0; i < courses.length - 1; i++) {
+        for (var j = i+1; j < courses.length; j++) {
+            if (courses[i]["code"] === courses[j]["code"]){
+                return false;
+            }
+            if (!compareTime(courses[i], courses[j])) { 
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+// get combinations of courses
+function k_combinations(set, k) {
+	var i, j, combs, head, tailcombs;
+	
+	if (k > set.length || k <= 0) {
+		return [];
+	}
+
+	if (k == set.length) {
+		return [set];
+	}
+
+	if (k == 1) {
+		combs = [];
+		for (i = 0; i < set.length; i++) {
+			combs.push([set[i]]);
+		}
+		return combs;
+	}
+	
+	combs = [];
+	for (i = 0; i < set.length - k + 1; i++) {
+		head = set.slice(i, i + 1);
+        var nextIdx = 1;
+        while (i + nextIdx + k - 1 < set.length) {
+            if (set[i+nextIdx]["code"] === head[0]["code"])
+                nextIdx++;
+            else break;
+        }
+		tailcombs = k_combinations(set.slice(i + nextIdx), k - 1);
+
+		for (j = 0; j < tailcombs.length; j++) {
+			combs.push(head.concat(tailcombs[j]));
+		}
+	}
+	return combs;
+}
+
 function Timetable() {
     const [semester, setSemester] = useState("1");
     const [inputCheck, setInputCheck] = useState(false);
     const [selectedCourses, setSelectedCourses] = useState([]);
+    const [selectedSubclasses, setSelectedSubclasses] = useState([]);
     const [numCourse, setNumCourse] = useState("1");
+    const [make, setMake] = useState(false);
+    const [result, setResult] = useState([]);
 
     const handleSemesterChange = (e) => {
         if (e.target.value === "1") {
             setSemester("1");
             setSelectedCourses([]);
+            setSelectedSubclasses([]);
+            setMake(false);
         } else if (e.target.value === "2") {
             setSemester("2");
             setSelectedCourses([]);
+            setSelectedSubclasses([]);
+            setMake(false);
         }
     }
 
@@ -28,16 +102,36 @@ function Timetable() {
     };
 
     const handleAdd = (e) => {
-        setSelectedCourses(s => [...s, e]);
+        if(!selectedCourses.includes(e)){
+            var addedCourses = []
+            for(var i = 0; i<e["subclass"].length; i++){
+                var newCourse = {
+                    "code": e["code"],
+                    "title": e["title"],
+                    "term": e["term"],
+                    "section": e["subclass"][i]["section"],
+                    "stime": e["subclass"][i]["stime"],
+                    "etime": e["subclass"][i]["etime"],
+                    "venue": e["subclass"][i]["venue"],
+                    "day": e["subclass"][i]["day"]
+                }
+                addedCourses.push(newCourse);
+            }
+            setSelectedCourses(s => [...s, e]);
+            setSelectedSubclasses(s => [...s, ...addedCourses]);
+        }
     }
 
     const handleDelete = (e) => {
-        setSelectedCourses(prevS => prevS.filter(course => course.code+course.section != e.target.parentElement.id))
+        setSelectedCourses(prevS => prevS.filter(course => course.code != e.target.parentElement.id));
+        setSelectedSubclasses(prevS => prevS.filter(course => course.code != e.target.parentElement.id));
     }
 
     const handleMake = () => {
-        console.log(selectedCourses);
-        console.log(numCourse);
+        var comb = k_combinations(selectedSubclasses, numCourse);
+        comb = comb.filter(c => isValidCombination(c));
+        setResult(comb);
+        setMake(true);
     }
 
     return(
@@ -60,7 +154,7 @@ function Timetable() {
             {selectedCourses.length > 0 ? 
             <ul>
                 {selectedCourses.map((course,idx) => 
-                    <li id={course.code+course.section} key={idx}>
+                    <li id={course.code} key={idx}>
                         <span>{course.code + " " + course.title}</span>
                         <button onClick={handleDelete}>delete</button>
                     </li>
@@ -71,6 +165,15 @@ function Timetable() {
                 <label htmlFor="course-num">Number of courses to take: </label>
                 <input name="course-num" value={numCourse} type="number" min="1" max={selectedCourses.length > 5 ? 5 : selectedCourses.length} onChange={(e)=>setNumCourse(e.target.value)}/>
                 <button onClick={handleMake}>Make timetable</button>
+            </div>
+            : null}
+            {make ? 
+            <div>
+                {result.map((comb,idx) =>
+                    <ul key={idx}>
+                        {comb.map((course,id) => <li key={id}>{course.code + " " + course.title + " " + course.section + " " + course.day + " " + course.stime + " " + course.etime}</li>)}
+                    </ul>   
+                )}
             </div>
             : null}
         </div>
