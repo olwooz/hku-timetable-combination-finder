cbf311363955d38be46fec7833dfe76e0c2f41b7
dyi919 <ehddjs13579@gmail.com>
2021-12-31 21:51:59 +0900

    Added Course Select and No. of Courses to Take

--- src/routes/Timetable.js
+++ src/routes/Timetable.js
@@ -1,12 +1,73 @@
-import data from "../data/simplified.json";
+import { useEffect, useState } from "react";
+import Select from 'react-select'
+
+import courses from "../data/simplified";
 
 function Timetable() {
-    console.log(data)
+    const [semester, setSemester] = useState("1");
+    const [inputCheck, setInputCheck] = useState(false);
+    const [selectedCourses, setSelectedCourses] = useState([]);
+
+    const handleSemesterChange = (e) => {
+        if (e.target.value === "1") {
+            setSemester("1");
+            setSelectedCourses([]);
+        } else if (e.target.value === "2") {
+            setSemester("2");
+            setSelectedCourses([]);
+        }
+    }
+
+    const handleInputChange = (e) => {
+        if (e.length > 3) {
+            setInputCheck(true);
+        } else {
+            setInputCheck(false);
+        }
+    };
+
+    const handleAdd = (e) => {
+        setSelectedCourses(s => [...s, e]);
+    }
+
+    const handleDelete = (e) => {
+        setSelectedCourses(prevS => prevS.filter(course => course.code+course.section != e.target.parentElement.id))
+    }
+
     return(
         <div>
             <h1>Timetable</h1>
+            <div>
+                Semester 1 <input type="radio" value="1" name="semester" checked={semester === "1" ? true : false} onChange={handleSemesterChange}/>
+                Semester 2 <input type="radio" value="2" name="semester" checked={semester === "2" ? true : false} onChange={handleSemesterChange}/>
+            </div>
+            <Select 
+                name="selectCourse"
+                value=""
+                options={inputCheck ? courses.filter(course => course.term == semester) : []}
+                onInputChange={handleInputChange}
+                getOptionLabel={(course) => course.code + " (" + course.title + ")"}
+                getOptionValue={(course) => course.code + course.section}
+                placeholder="Type to search by course code or name..."
+                onChange={handleAdd}
+            />
+            {selectedCourses.length > 0 ? 
+            <ul>
+                {selectedCourses.map((course,idx) => 
+                    <li id={course.code+course.section} key={idx}>
+                        <span>{course.code + " " + course.title}</span>
+                        <button onClick={handleDelete}>delete</button>
+                    </li>
+                )}
+            </ul> : null}
+            {selectedCourses.length > 0 ? 
+            <div>
+                <label for="course-num">Number of courses to take: </label>
+                <input name="course-num" type="number" min="1" max={selectedCourses.length > 5 ? 5 : selectedCourses.length }/>
+            </div>
+            : null}
         </div>
     );
 }
 
-export default Timetable
+export default Timetable;
