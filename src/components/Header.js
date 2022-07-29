89c76a9c20d154317a72784e52b1b906cb083a7f
dyi919 <ehddjs13579@gmail.com>
2022-07-30 00:24:22 +0900

    added hits

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,6 +20,9 @@ function Header() {
         <Typography variant="h2">
           HKU Timetable Combination Finder (22-23)
         </Typography>
+        <a href="https://hits.seeyoufarm.com">
+          <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fdyi919.github.io%2Fhku-timetable-combination-finder%2F&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false" />
+        </a>
       </ThemeProvider>
     </div>
   );
