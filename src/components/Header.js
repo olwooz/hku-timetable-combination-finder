5ae4cd4d85c5207c87b79363bac48430d527c4d5
dyi919 <ehddjs13579@gmail.com>
2022-07-29 23:48:56 +0900

    header updated for 22-23 academic year

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,28 @@
 import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme();
 
 theme.typography.h2 = {
-  fontSize: '1.2rem',
-  '@media (min-width:600px)': {
-    fontSize: '1.5rem',
+  fontSize: "1.2rem",
+  "@media (min-width:600px)": {
+    fontSize: "1.5rem",
   },
-  [theme.breakpoints.up('md')]: {
-    fontSize: '2.4rem',
+  [theme.breakpoints.up("md")]: {
+    fontSize: "2.4rem",
   },
 };
 
-function Header(){
-    return(
-        <div className="header-wrapper">
-            <ThemeProvider theme={theme}>
-                <Typography variant="h2">HKU Timetable Combination Finder (21-22)</Typography>
-            </ThemeProvider>
-        </div>
-    );
+function Header() {
+  return (
+    <div className="header-wrapper">
+      <ThemeProvider theme={theme}>
+        <Typography variant="h2">
+          HKU Timetable Combination Finder (22-23)
+        </Typography>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default Header;
